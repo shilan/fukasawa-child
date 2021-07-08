5d6b362c6827cc2ded30ddaa26ab335fea077776
Shilan Kalhor <shika@danskebank.com>
2021-07-08 10:28:15 +0200

    size and price separately.

--- singular.php
+++ singular.php
@@ -79,8 +79,13 @@
 							$size = get_post_meta($post->ID, 'size', true);
 							if ($size) {
 						?>
-							<p class="custom_field size"><label>Size:</label> <?php echo get_post_meta($post->ID, 'size', true); ?> cm</p>
-							<p class="custom_field price"><label>Price:</label> USD <?php echo get_post_meta($post->ID, 'price', true); ?></p>
+							<p class="custom_field size"><label>Size:</label> <?php echo $size; ?> cm</p>
+							<?php
+							}
+							$price = get_post_meta($post->ID, 'price', true);
+							if ($price) { 	
+							?>
+							<p class="custom_field price"><label>Price:</label> USD <?php  echo $price ?></p>
 						<?php
 							}
 							the_content();
